a2486b51ecf5b356fee482859d8dd23fd62fca21
Atze Cervantes <Atzelcer@gmail.com>
2023-12-24 21:39:32 -0400

    destruccion

--- lavaca.cpp
+++ lavaca.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 cout << "Las vacas no vuelan " << endl;
-cout << "No me dare porvencido jamas de los jamaces en vida << endl;
+cout << "No me dare porvencido jamas de los jamaces en vida "<< endl;
 
 return 0;
 }
